e0a68bb1f0a1d0f7fc63c96368f51146f02502bb
khobayer <khobayerkhan@gmail.com>
2024-12-17 18:34:09 +0600

    Refactor - entity interfaces

--- src/components/GameAttributes.tsx
+++ src/components/GameAttributes.tsx
@@ -1,5 +1,5 @@
 import { Text, SimpleGrid } from "@chakra-ui/react";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 import CriticScore from "./CriticScore";
 import DefinitionItem from "./DefinitionItem";
 
